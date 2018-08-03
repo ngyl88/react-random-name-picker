2067afb08972a3face966c97267be9f8c50a11c3
ngyl88 <ng_yl88@hotmail.com>
2018-08-03 18:12:16 +0800

    app testing with react-test-renderer and jest-mock

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ShallowRenderer from "react-test-renderer/shallow";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// ES6 mocking
+import { randomInt } from "mathjs";
+jest.mock("mathjs", () => {
+  return {
+    randomInt: jest.fn()
+  };
+});
+
+it("renders App without crashing", () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+
+  const output = renderer.getRenderOutput();
+  expect(output).toMatchSnapshot();
+});
+
+it("renders App correctly after addNewName is called", () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+
+  const instance = renderer.getMountedInstance();
+  instance.addNewName("Christmas");
+
+  const output = renderer.getRenderOutput();
+  expect(output).toMatchSnapshot();
+});
+
+it("renders App correctly after handleClick is called", () => {
+  // randomInt.mockReset();
+  randomInt.mockImplementationOnce(num => 2);
+
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+
+  const instance = renderer.getMountedInstance();
+  instance.handleClick();
+
+  const output = renderer.getRenderOutput();
+  expect(output).toMatchSnapshot();
 });
